790e8b3ad4dc9ed054711a195f472965bde8b83a
Zachary Guard <zacharyguard@gmail.com>
2014-04-07 11:38:46 +0800

    First commit

---
+++ membrane.js
@@ -0,0 +1,49 @@
+var $$ = function(sel) {
+    return document.querySelector(sel);
+}
+ 
+var $$$ = function(sel) {
+    return document.querySelectorAll(sel);
+}
+
+Array.prototype.contains = function(q){if(this.indexOf(q) != -1){return true}else{return false}}
+
+var scriptInject = function(uri){
+    var _injectJS_script = document.createElement("script");
+    _injectJS_script.src = uri;
+    document.querySelectorAll("head")[0].appendChild(_injectJS_script);
+}
+
+var objectSort = function(array,sortKey,sortFunction) {
+    switch (sortFunction) {
+        case "number-asc":
+            return array.sort(function(a, b){
+                return a[sortKey]-b[sortKey];
+            });
+            break;
+        case "number-desc":
+            return array.sort(function(a, b){
+                return b[sortKey]-a[sortKey];
+            });
+            break;
+        case "alpha-desc":
+            return array.sort(function(a,b){
+                if(a[sortKey].toLowerCase() < b[sortKey].toLowerCase()) return 1;
+                if(a[sortKey].toLowerCase() > b[sortKey].toLowerCase()) return -1;
+                return 0;
+            });
+            break;
+        default:
+            return array.sort(function(a,b){
+                if(a[sortKey].toLowerCase() < b[sortKey].toLowerCase()) return -1;
+                if(a[sortKey].toLowerCase() > b[sortKey].toLowerCase()) return 1;
+                return 0;
+            });
+            break;
+    }
+}
+
+var gotoElem = function(sel) {
+    var targetElem = sel; // must be an element object, not a selector string
+    window.scrollTo(targetElem.offsetLeft+targetElem.offsetWidth/2-window.innerWidth/2,targetElem.offsetTop+targetElem.offsetHeight/2-window.innerHeight/2);
+}
