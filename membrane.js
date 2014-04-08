8c413cf8767f417dd731fecf468aa7e5adbc3bc7
Zachary Guard <zacharyguard@gmail.com>
2014-04-08 10:36:11 +0800

    Added parseURL function

--- membrane.js
+++ membrane.js
@@ -53,3 +53,40 @@ var addStyle = function(sel,style) {
     newStylesheet.innerHTML = sel+"{"+style+"}";
     $$$("head")[0].appendChild(newStylesheet);
 }
+
+String.prototype.parseURL = function(target) {
+    var parser = document.createElement("a");
+    parser.href = this;
+    switch (target) {
+        case "protocol":
+            return parser.protocol; break;
+        case "hostname":
+            return parser.hostname; break;
+        case "port":
+            return parser.port; break;
+        case "path":
+            return parser.pathname; break;
+        case "patharray":
+            var _pathstring = parser.pathname.substring(1,this.length-1); // cut off leading and ending slashes (/)
+            return _pathstring.split("/"); break;
+        case "params":
+            return parser.search; break;
+        case "paramsarray":
+            var _urlparams = parser.search.substring(1,parser.search.length).split("&"); // ["foo=bar","bax=qux","waffles=pie"]
+            var _paramsobject = [];
+            for (i=0;i<_urlparams.length;i++) {
+                _paramsobject.push({});
+                var _key = _urlparams[i].split("=")[0];
+                var _val = _urlparams[i].split("=")[1];
+                _paramsobject[i][_key] = _val;
+            };
+            return _paramsobject;
+            break;
+        case "hash":
+            return parser.hash; break;
+        case "host":
+            return parse.host; break;
+        default:
+            return parser.href; break;
+    }
+}
